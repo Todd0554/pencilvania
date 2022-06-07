2db0e0395bb8ac716ce0bb54a5676483366b3fce
Todd0554 <toddy9229@outlook.com>
2022-06-07 21:04:19 +1000

    radio button and select form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
-
+  before_action :set_form_vars
   # GET /listings or /listings.json
   def index
     @listings = Listing.all
@@ -62,6 +62,12 @@ class ListingsController < ApplicationController
     def set_listing
       @listing = Listing.find(params[:id])
     end
+
+    def set_form_vars
+      @categories = Category.all
+      @conditions = Listing.conditions.keys
+    end
+
 
     # Only allow a list of trusted parameters through.
     def listing_params
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+  
+  enum condition: { 
+    last_legs: 1,
+    worn_out: 2,
+    fresh_and_shiny: 3,
+    used_once: 4
+   }
 end
